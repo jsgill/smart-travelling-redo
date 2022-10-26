849e0e86095ca31e58d3ff9ace2e03f0b4cd13f0
seemakaur11 <raiseema248@gmail.com>
2022-10-26 16:10:50 +0530

    trip three section issuw fixed

--- components/TripInput.jsx
+++ components/TripInput.jsx
@@ -14,7 +14,7 @@ const TripInput = ({ name, changeHandler, inputValue, max, className, id, clickH
             onClick={() => clickHandler && clickHandler()}
             onFocus={(e) => (e.target.value = "")}
             onBlur={(e) => e.target.value = inputValue}
-            readOnly
+
         />
     )
 }
