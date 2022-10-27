0e885e19c3d442fb61ae0e0276c6a3b23ea5a47e
Mukesh97297 <mkverma97297@gmail.com>
2022-10-27 08:41:25 +0530

    tripSubmit button homeWorksSection and homeCard sectiom

--- components/HomeEProps.jsx
+++ components/HomeEProps.jsx
@@ -6,10 +6,10 @@ const HomeEProps = ({ para, title, imgPath, rowStru, higCus, widCus, cusCss, id 
     return (
         <>
             <div className={`row ${rowStru} mb-5`}>
-                <div className={`col-md-8 ${cusCss}`} >
+                <div className={`col-md-8 ${cusCss}`} id={styles.worksImageAlignment}>
                     <Image src={imgPath} height={higCus} width={widCus} alt="angle" />
                 </div>
-                <div className='col-md-4'>
+                <div className='col-md-4' id={styles.worksImageAlignment}>
                     <div className={styles.home_needs1}>
                         <h1>{title}</h1>
                         <p>{para}</p>
