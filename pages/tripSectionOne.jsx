112451bf6ac0ac473137b005446315bead16c665
Mukesh97297 <mkverma97297@gmail.com>
2022-10-29 10:54:58 +0530

    tripone

--- pages/tripSectionOne.jsx
+++ pages/tripSectionOne.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect, useRef } from "react";
 import Link from "next/link";
 import styles from "../styles/TripHeader.module.css";
 import {
